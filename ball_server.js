1c23a5234499c04b8f514f4ad5a3072fd099f8ef
Eve Weinberg <eve@neveroddoreven.tv>
2016-10-31 18:09:18 -0400

    first

--- ball_server.js
+++ ball_server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var server = express();
 
 //create a folder where you can build html front end stuff
-//the root of the statis stuff lives in public
+//the root of the static stuff lives in public
 server.use('/', express.static('public'));
 //there will be URL encouded information ''?thing=value'
 server.use(bodyParser.urlencoded({extended:true}))
@@ -11,11 +11,30 @@ server.use(bodyParser.urlencoded({extended:true}))
 server.listen(8080);
 server.get('/play', responseToClient)
 server.get('/bye', anotherThingy)
-server.post('/requests', doPost)
+// server.post('/requests', doPost)
+
+const net = require('net');
+// 172.22.151.124
+const client = net.connect("8080","172.16.252.152",function(){
+	console.log("connected to server!");
+
+	client.write('n=E++++++M\n');
+	client.write('i');
 
 
+});
+
+setInterval(function(){
+  var thing = ['u', 'l', 'r', 'd']
+  client.write(thing[Math.floor(Math.random(thing.length))]);
+  console.log(thing[Math.floor(Math.random(thing.length))])
+  client.write('\r');
+  client.write('\n');
+
+},1000)
 
 function responseToClient(request, response){
+
   console.log(request.query)
   if (request.query.instruction == 'L'){
     console.log('left')
@@ -24,7 +43,7 @@ function responseToClient(request, response){
   }
   //respond to client and lets server know we have something
   console.log(request)
-  response.end("Hello, cleint!")
+  response.end("Hello, client!")
 
   //get -
   //post - send info to the server, send the header then send data in the body of the request, used of r forms alot
@@ -32,8 +51,49 @@ function responseToClient(request, response){
 
 }
 
+server.post('/requests', function(req, res){
+    console.log('we hit test');
+
+    console.log(req.body);
+    // 108
+
+
+
+    // res.json({"msg":"success!"});
+    res.redirect('/index.html')
+    // res.end('thanks for the post!')
+    if(req.body.hasOwnProperty('down')){
+      console.log('down was hit')
+      client.write('d');
+      client.write('\r');
+      client.write('\n');
+    }
+    if(req.body.hasOwnProperty('left')){
+      console.log('left was hit')
+      client.write('l');
+      client.write('\r');
+      client.write('\n');
+    }
+
+    if(req.body.hasOwnProperty('right')){
+      console.log('right was hit')
+      client.write('r');
+      client.write('\r');
+      client.write('\n');
+    }
+
+    if(req.body.hasOwnProperty('up')){
+      console.log('up was hit')
+      client.write('u');
+      client.write('\r');
+      client.write('\n');
+    }
+    // client.write('l');
+});
+
+/*
 function doPost(request, response){
-  console.log(request.body)
+  // console.log(request.body)
   //the body of our request
   //this makes a json of the input from the index
   // console.log(request.body[down]);
@@ -58,6 +118,7 @@ function doPost(request, response){
   //   response.write('You are not old enough to play')
   // }
 }
+*/
 
 function anotherThingy(request,response){
     console.log(request.connection)
--- droplet_server.js
+++ droplet_server.js
@@ -1,21 +1,66 @@
+var express = require('express')
+var bodyParser = require('body-parser');
+var server = express();
+
+//create a folder where you can build html front end stuff
+//the root of the statis stuff lives in public
+server.use('/', express.static('public'));
+server.post('/requests', doPost)
+server.listen(8080);
+server.use(bodyParser.urlencoded({extended:true}))
+
+server.get('/up', upF)
+
+function upF(){
+	client.write('u');
+	console.log('up window opwn')
+
+}
 
 const net = require('net');
-const client = net.connect("8080","172.22.151.124",function(){
+// 172.22.151.124
+const client = net.connect("8080","172.16.252.152",function(){
 	console.log("connected to server!");
 
-	client.write('n=' + 'E + M' +'\n');
+	client.write('n=' + 'E++++++M' +'\n');
 	client.write('i');
 
-	// setInterval(function(){
-// },2000);
-
-		//if we hear form the front end that L happened
-		//client.write('l');
-		//if we hear form the front end that r happened
-		//client.write('r');
-		//if we hear form the front end that u happened
-		//client.write('u');
-		//if we hear form the front end that d happened
-		//client.write('d');
-		
 });
+
+server.get('/test', function(req,res){
+
+	console.log('we hit the "test" endpoint');
+
+});
+
+
+function doPost(request, response){
+	  console.log(request.body)
+	  //the body of our request
+	  //this makes a json of the input from the index
+	  // console.log(request.body[down]);
+	  // if(request.body.hasOwnProperty('down')){
+		// 	client.write('d');
+	  //   console.log('down was hit')
+	  // }
+	  // if(request.body.hasOwnProperty('left')){
+		// 		client.write('l');
+	  //   console.log('left was hit')
+	  // }
+		//
+	  // if(request.body.hasOwnProperty('right')){
+	  //   console.log('right was hit')
+		// 	client.write('r');
+	  // }
+		//
+	  // if(request.body.hasOwnProperty('up')){
+	  //   console.log('up was hit')
+		// 	client.write('u');
+	  // }
+	  // // response.write('<p>Hi</p>')
+	  // response.redirect('/index.html')
+	  // response.end('thanks for the post!')
+	  // if (request.query.age < 21){
+	  //   response.write('You are not old enough to play')
+	  // }
+	}
